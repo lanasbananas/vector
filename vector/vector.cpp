378b4d567d28b3504b62de98308f3fbcffd85ec9
Лана Вартанова <lanasbananasss@gmail.com>
2021-11-07 18:34:03 +0400

    поправки

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -1,9 +1,12 @@
 ﻿#include <iostream>
 #include <stdio.h>
+
 using namespace std;
 
 class vector
 {
+private:
+
 	size_t _size;
 	double* _data;
 
@@ -22,13 +25,15 @@ public:
 	vector(size_t size, double data)  //констр копирования
 	{
 		_size = size;
+		if (_size < 0)
+			throw("Can not negative size");
 		_data = new double[_size];
 		for (size_t i = 0; i < _size; i++)
 		{
 			_data[i] = data;
 		};
-	};
-	
+	}; //конструктор копирования копирует данные в пустой объект (он только создается та ниче нет)
+
 	~vector() //деструктор
 	{
 		delete[] _data;
@@ -41,23 +46,39 @@ public:
 		return _data[data];
 	};
 
-	vector& operator = (const vector& v) //оператор присваивания
+	vector(const vector& v)
+	{
+		_size = v._size;
+		delete[] _data;
+		_data = new double[_size];
+		for (size_t i = 0; i < _size; i++)
+			_data[i] = v._data[i];
+	}
+
+	vector(vector&& move) //чтобы работало + и -
+	{ 
+		_data = move._data;
+		_size = move._size;
+		move._data = nullptr;
+		move._size = 0;
+	} 
+	vector& operator= (const vector& v) //оператор присваивания
 	{
 		//проверка на самоприсваивание
 		if (this == &v)
 			return (*this);
 		if (_size == v._size)			///////////
-		{
-			delete[]_data;
+		{                               //прежде чем выполнять надо выполнить осовобождение памяти
+			delete[]_data;              //на которую указывает указатель дата
 			_size = v._size;
 			_data = new double[v._size];
 		};
-		for (int i = 0; i < _size; i++)
+		for (size_t i = 0; i < _size; i++)
 		{
 			_data[i] = v._data[i];
-		};
-		return (*this);
-	};
+		};               //возвращает ссылку на текущий объект
+		return (*this); //разыменование оператора this
+	}; //this - текущий оюъект класса
 
 	vector operator+(const vector& v1) //сложение
 	{
@@ -85,7 +106,7 @@ public:
 		};
 		return dif;
 	};
-	
+
 	double operator*(const vector& v1) //скалярное произведение
 	{
 		if (_size != v1._size)
@@ -97,8 +118,8 @@ public:
 		};
 		return mul;
 	};
-	
-	vector operator*(const double data)
+
+	vector operator*(const double data) //умножение на скаляр
 	{
 		vector res(_size, 0);
 		for (size_t i = 0; i < _size; i++)
@@ -107,7 +128,7 @@ public:
 		};
 		return res;
 	};
-
+	
 	vector operator/(const double data)
 	{
 		if (data == 0)
@@ -119,4 +140,44 @@ public:
 		};
 		return res;
 	};
+	
+	friend istream& operator>> (istream& is, vector& v) //оператор извлечния >> (istream-входной поток)
+	{
+		
+		cout << "Size: ";
+		is >> v._size;
+		v._data = new double[v._size];
+		for (size_t i = 0; i < v._size; i++)
+			is >> v._data[i];
+		return is;
+	};
+
+	friend ostream& operator<< (ostream& os, const vector& v) //оператор вставки << (ostream-выходной поток)
+	{
+		os << "[ ";
+		for (size_t i = 0; i < v._size; i++)
+			os << v._data[i] << " ";
+		os << "]" << endl;
+		return os;
+	}; 
+
+};
+
+int main() 
+{
+	vector a;
+	vector b;
+	int num = 6;
+	cin >> a;
+	cin >> b;
+	cout << "a = " << a;
+	cout << "b = " << b;
+	cout << "a + b = " << a + b;
+	cout << "a - b = " << a - b;
+	cout << "a * b = " << a * b;
+	cout << "\na * num = " << a * num;
+	cout << "b * num = " << b * num;
+	cout << "a / num = " << a / num;
+	cout << "b / num = " << b / num;
+	return 0;
 }
