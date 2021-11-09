b1817cf53bb43abbf10d75993adcc900b40ae515
Лана Вартанова <lanasbananasss@gmail.com>
2021-11-10 02:20:40 +0400

    на сдачу

--- vector/vector.cpp
+++ vector/vector.cpp
@@ -9,12 +9,12 @@ private:
 
 	size_t _size;
 	double* _data;
-
+	
 public:
 
-	vector()
+	 vector()
 	{
-		_size = 2;
+		_size = 2; 
 		_data = new double[_size];
 		for (size_t i = 0; i < _size; i++)
 		{
@@ -22,7 +22,7 @@ public:
 		};
 	};
 
-	vector(size_t size, double data)  //констр копирования
+	vector(size_t size, double data) 
 	{
 		_size = size;
 		if (_size < 0)
@@ -32,82 +32,71 @@ public:
 		{
 			_data[i] = data;
 		};
-	}; //конструктор копирования копирует данные в пустой объект (он только создается та ниче нет)
-
-	~vector() //деструктор
+	}; 
+	
+	~vector() 
 	{
 		delete[] _data;
 	};
 
-	double operator[](size_t data) //оператор чтения (с выводом на экран) вектора по указанному индексу
+	double& operator[](size_t ind) 
 	{
-		if (data >= _size || data < 0)
+		if (ind >= _size || ind < 0)
 			throw std::logic_error("Invalid index");
-		return _data[data];
+		return _data[ind];
 	};
 
-	vector(const vector& v)
+	vector(vector&& move) noexcept 
 	{
-		_size = v._size;
-		delete[] _data;
-		_data = new double[_size];
-		for (size_t i = 0; i < _size; i++)
-			_data[i] = v._data[i];
-	}
-
-	vector(vector&& move) //чтобы работало + и -
-	{ 
 		_data = move._data;
 		_size = move._size;
 		move._data = nullptr;
 		move._size = 0;
-	} 
-	vector& operator= (const vector& v) //оператор присваивания
+	};
+
+	vector& operator= (const vector& v) 
 	{
-		//проверка на самоприсваивание
 		if (this == &v)
-			return (*this);
-		if (_size == v._size)			///////////
-		{                               //прежде чем выполнять надо выполнить осовобождение памяти
-			delete[]_data;              //на которую указывает указатель дата
-			_size = v._size;
-			_data = new double[v._size];
-		};
+			return (*this);   
+		delete[]_data;              
+		_size = v._size;
+		_data = new double[_size];
 		for (size_t i = 0; i < _size; i++)
 		{
 			_data[i] = v._data[i];
-		};               //возвращает ссылку на текущий объект
-		return (*this); //разыменование оператора this
-	}; //this - текущий оюъект класса
+		};             
+		return (*this);
+	};                 
 
-	vector operator+(const vector& v1) //сложение
+	vector operator+(const vector& v1) 
 	{
 		if (_size != v1._size)
 			throw("Can not sum2 vector!");
 		vector sum;
 		sum._size = _size;
 		sum._data = new double[_size];
-		for (int i = 0; i < _size; i++)
+		for (size_t i = 0; i < _size; i++)
 		{
 			sum._data[i] = _data[i] + v1._data[i];
 		};
 		return sum;
 	};
 
-	vector operator-(const vector& v1) //вычитание
+	vector operator-(const vector& v1) 
 	{
-		if (_size != v1._size)
+		if (_size != v1._size) 
 			throw "Can not dif2 vector!";
 		vector dif;
 		dif._size = _size;
-		for (int i = 0; i < _size; i++)
+		dif._data = new double[_size];
+		for (size_t i = 0; i < _size; i++)
 		{
 			dif._data[i] = _data[i] - v1._data[i];
 		};
 		return dif;
 	};
-
-	double operator*(const vector& v1) //скалярное произведение
+	
+	double operator*(const vector& v1) 
 	{
 		if (_size != v1._size)
 			throw "Can not mul2 vector!";
@@ -119,12 +108,12 @@ public:
 		return mul;
 	};
 
-	vector operator*(const double data) //умножение на скаляр
+	vector operator*(const double a)
 	{
 		vector res(_size, 0);
 		for (size_t i = 0; i < _size; i++)
 		{
-			res._data[i] = _data[i] * data;
+			res._data[i] = _data[i] * a;
 		};
 		return res;
 	};
@@ -141,7 +130,7 @@ public:
 		return res;
 	};
 	
-	friend istream& operator>> (istream& is, vector& v) //оператор извлечния >> (istream-входной поток)
+	friend istream& operator>> (istream& is, vector& v) 
 	{
 		
 		cout << "Size: ";
@@ -152,7 +141,7 @@ public:
 		return is;
 	};
 
-	friend ostream& operator<< (ostream& os, const vector& v) //оператор вставки << (ostream-выходной поток)
+	friend ostream& operator<< (ostream& os, const vector& v) 
 	{
 		os << "[ ";
 		for (size_t i = 0; i < v._size; i++)
@@ -162,22 +151,3 @@ public:
 	}; 
 
 };
-
-int main() 
-{
-	vector a;
-	vector b;
-	int num = 6;
-	cin >> a;
-	cin >> b;
-	cout << "a = " << a;
-	cout << "b = " << b;
-	cout << "a + b = " << a + b;
-	cout << "a - b = " << a - b;
-	cout << "a * b = " << a * b;
-	cout << "\na * num = " << a * num;
-	cout << "b * num = " << b * num;
-	cout << "a / num = " << a / num;
-	cout << "b / num = " << b / num;
-	return 0;
-}
